9cac8a0c2a4cf9cf2be7b048568352386d59f85f
duarte171183 <omardj17@gmail.com>
2016-04-10 12:34:30 -0500

    add price in collection select

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
 	has_many :sales
 	has_many :tickets, :through => :sales
 	mount_uploader :photo, PhotoproductUploader
+
+	def Data_sales
+		"#{name} --> $#{price}"
+	end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,11 @@ class Ticket < ActiveRecord::Base
 	has_many :sales, :dependent => :destroy
 	has_many :products, :through => :sales
 	accepts_nested_attributes_for :sales, :reject_if => :all_blank, :allow_destroy=> true 
+
+	attr_accessor :status
+
+	def initialize      
+   	 	@status = 1
+    	super
+  	end
 end
