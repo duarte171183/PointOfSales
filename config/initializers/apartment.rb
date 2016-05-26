065e3832fcd8264a3f2bb051aab3ca767e575717
omar duarte juan <omardj17@gmail.com>
2016-05-26 00:15:17 -0500

    add ruby version

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -64,4 +64,4 @@ end
 # }
 
 # Rails.application.config.middleware.use 'Apartment::Elevators::Domain'
-Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
+#Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
