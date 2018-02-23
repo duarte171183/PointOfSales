a979139dea989e59e00e60af00359db284a538b2
omar duarte <omardj17@gmail.com>
2018-02-23 13:16:25 -0600

    add ticket open win

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,8 @@
 //= require select2
 //= require select2-full
 
+
+
 function formatRepo (repo) {
    
     if (repo.loading) return repo.text;
@@ -46,6 +48,7 @@ function formatRepo (repo) {
   function formatRepoSelection (repo) {
     return repo.name || repo.text;
   }
+
 
 $(document).ready(function() {
   $('#order_supplier_id').select2({
@@ -120,94 +123,3 @@ $(document).on('nested:fieldAdded', function(event){
   templateSelection: formatRepoSelection
   });
 })
-
-
-/*$(".js-example-data-ajax").select2({
-  ajax: {
-    url: "https://api.github.com/search/repositories",
-    dataType: 'json',
-    delay: 250,
-    data: function (params) {
-      return {
-        q: params.term, // search term
-        page: params.page
-      };
-    },
-    processResults: function (data, params) {
-      // parse the results into the format expected by Select2
-      // since we are using custom formatting functions we do not need to
-      // alter the remote JSON data, except to indicate that infinite
-      // scrolling can be used
-      params.page = params.page || 1;
-
-      return {
-        results: data.items,
-        pagination: {
-          more: (params.page * 30) < data.total_count
-        }
-      };
-    },
-    cache: true
-  },
-  placeholder: 'Search for a repository',
-  escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
-  minimumInputLength: 1,
-  templateResult: formatRepo,
-  templateSelection: formatRepoSelection
-});
-
-function formatRepo (repo) {
-  if (repo.loading) {
-    return repo.text;
-  }
-
-  var markup = "<div class='select2-result-repository clearfix'>" +
-    "<div class='select2-result-repository__avatar'><img src='" + repo.owner.avatar_url + "' /></div>" +
-    "<div class='select2-result-repository__meta'>" +
-      "<div class='select2-result-repository__title'>" + repo.full_name + "</div>";
-
-  if (repo.description) {
-    markup += "<div class='select2-result-repository__description'>" + repo.description + "</div>";
-  }
-
-  markup += "<div class='select2-result-repository__statistics'>" +
-    "<div class='select2-result-repository__forks'><i class='fa fa-flash'></i> " + repo.forks_count + " Forks</div>" +
-    "<div class='select2-result-repository__stargazers'><i class='fa fa-star'></i> " + repo.stargazers_count + " Stars</div>" +
-    "<div class='select2-result-repository__watchers'><i class='fa fa-eye'></i> " + repo.watchers_count + " Watchers</div>" +
-  "</div>" +
-  "</div></div>";
-
-  return markup;
-}
-
-function formatRepoSelection (repo) {
-  return repo.full_name || repo.text;
-}*/
-// Set up the Select2 control
-// $('#mySelect2').select2({
-//     ajax: {
-//         url: '/api/students'
-//     }
-// });
-
-// // Fetch the preselected item, and add to the control
-// var studentSelect = $('#mySelect2');
-// $.ajax({
-//     type: 'GET',
-//     url: '/api/students/s/' + studentId
-// }).then(function (data) {
-//     // create the option and append to Select2
-//     var option = new Option(data.full_name, data.id, true, true);
-//     studentSelect.append(option).trigger('change');
-
-//     // manually trigger the `select2:select` event
-//     studentSelect.trigger({
-//         type: 'select2:select',
-//         params: {
-//             data: data
-//         }
-//     });
-// });
-
-
-  
